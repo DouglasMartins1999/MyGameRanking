ae7a915b580629d7395073252b562abfad2251a9
Douglas Martins <douglas.martins.m099@gmail.com>
2019-09-18 12:06:43 -0300

    API Fetch Implemented

---
+++ MyGameRanking/Services/GamesFetch.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class GamesFetch {
+    static func searchGame(_ query: String, callback: @escaping ([IGDBList]) -> ()){
+        if let url:URL = URL(string: "https://api-v3.igdb.com/games/") {
+            var urlRequest:URLRequest = URLRequest(url: url);
+            urlRequest.httpMethod = "GET"
+            urlRequest.httpBody = "search \"\(query)\"; fields name, release_dates.y, platforms.name, genres.name, cover.height, cover.width, cover.url, involved_companies.company.name,  involved_companies.developer , aggregated_rating, rating, game_modes.name, player_perspectives, time_to_beat.normally, age_ratings.rating, artworks.url;".data(using: .utf8)
+            urlRequest.setValue("06e6fa9873ef43137e415a554480d8e1", forHTTPHeaderField: "user-key")
+            
+            URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                if error == nil {
+                    if let data = data {
+                        if let res:[IGDBList] = try? JSONDecoder().decode(Array<IGDBList>.self, from: data){
+                            callback(res)
+                        }
+                    }
+                } else {
+                    print(error)
+                }
+            }.resume()
+        }
+    }
+}
